0a2a04083c18eb9ff029ab2b27fb217ab7e240a8
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-01 05:40:56 -0400

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -197,9 +197,11 @@ void OverviewPage::updatBlockChainInfo()
     int CurrentBlock = (int)chainActive.Height();
     int64_t BlockReward = GetBlockValue(chainActive.Height(), tip_time);
     double BlockRewardHTH =  static_cast<double>(BlockReward)/static_cast<double>(COIN);
+    double CurrentDiff = GetDifficulty();
 
     ui->label_CurrentBlock_value->setText(QString::number(CurrentBlock));
-
+    ui->label_Nethash_3->setText(tr("Difficulty:"));
+    ui->label_Nethash_value_3->setText(QString::number(CurrentDiff,'f',4));
     ui->label_CurrentBlockReward_value->setText(QString::number(BlockRewardHTH));
 }
 }
