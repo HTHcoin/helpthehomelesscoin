9591fee58cf0edeebdd153b5f953e07af3b1edc3
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-03 21:46:56 -0400

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -197,7 +197,7 @@ void OverviewPage::updateBlockChainInfo()
     uint32_t tip_time = chainActive.Tip()->GetBlockTime();
 
     int CurrentBlock = (int)chainActive.Height();
-    int64_t BlockReward = GetBlockValue(chainActive.Height(), tip_time);
+    int64_t BlockReward = blockReward(chainActive.Height(), tip_time);
     double BlockRewardHTH =  static_cast<double>(BlockReward)/static_cast<double>(COIN);
     double CurrentDiff = GetDifficulty();
   
