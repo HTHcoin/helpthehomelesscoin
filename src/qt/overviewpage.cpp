ff99adcf8a809aab9aef321b91e305f01ebce67b
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-03 22:10:50 -0400

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -22,6 +22,7 @@
 #include "chainparams.h"
 #include "amount.h"
 #include "validation.h"
+#include "wallet.h"
 
 #include "instantx.h"
 #include "masternode-sync.h"
@@ -198,7 +199,7 @@ void OverviewPage::updateBlockChainInfo()
     uint32_t tip_time = chainActive.Tip()->GetBlockTime();
 
     int CurrentBlock = (int)chainActive.Height();
-    int64_t BlockReward = setBalance(chainActive.Height(), tip_time);
+    int64_t BlockReward = GetBalance(chainActive.Height(), tip_time);
     double BlockRewardHTH =  static_cast<double>(BlockRewardHTH)/static_cast<double>(COIN);
     double CurrentDiff = GetDifficulty();
   
