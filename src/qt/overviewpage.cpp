92d490edaa6111b7d2e132df3c2f228b1bfede0d
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-13 14:36:01 -0400

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -141,7 +141,7 @@ void OverviewPage::updateWatchOnlyLabels(bool showWatchOnly)
 {
     ui->labelSpendable->setVisible(showWatchOnly);      // show spendable label (only when watch-only is active)
     ui->labelWatchonly->setVisible(showWatchOnly);      // show watch-only label
-    ui->labelwatchBalance->setVisible(showWatchOnly);    // show watch-only balance separator line
+    ui->labelWatchBalance->setVisible(showWatchOnly);    // show watch-only balance separator line
     ui->labelWatchAvailable->setVisible(showWatchOnly); // show watch-only available balance
     ui->labelWatchPending->setVisible(showWatchOnly);   // show watch-only pending balance
     ui->labelWatchTotal->setVisible(showWatchOnly);     // show watch-only total balance
