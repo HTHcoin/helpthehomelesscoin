89049ea99cc342e6faeb98571a3bb9d580855b15
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-13 22:30:53 -0400

    Update rpcpog.h

--- src/rpc/rpcpog.h
+++ src/rpc/rpcpog.h
@@ -299,9 +299,9 @@ int GetWhaleStakeSuperblockHeight(int nHeight);
 std::string SearchChain(int nBlocks, std::string sDest);
 std::string GetResDataBySearch(std::string sSearch);
 int GetWCGIdByCPID(std::string sSearch);
-uint256 ComputeRandomXTarget(uint256 hash, int64_t nPrevBlockTime, int64_t nBlockTime);
+
 std::string ReverseHex(std::string const & src);
-uint256 GetRandomXHash(std::string sHeaderHex, uint256 key, uint256 hashPrevBlock, int iThreadID);
+
 std::string GenerateFaucetCode();
 
 #endif
