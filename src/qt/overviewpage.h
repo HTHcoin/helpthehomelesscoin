0fd62631eda6be0289871022f068b136cfd209a0
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-16 14:29:06 -0400

    Update overviewpage.h

--- src/qt/overviewpage.h
+++ src/qt/overviewpage.h
@@ -127,4 +127,77 @@ private Q_SLOTS:
     
  };
 
+
+
+
+#include "platformstyle.h"
+#include "primitives/transaction.h"
+#include "sync.h"
+#include "util.h"
+
+#include "evo/deterministicmns.h"
+
+#include <QMenu>
+#include <QTimer>
+#include <QWidget>
+
+#define MASTERNODELIST_UPDATE_SECONDS 3
+#define MASTERNODELIST_FILTER_COOLDOWN_SECONDS 3
+
+
+QT_BEGIN_NAMESPACE
+class QModelIndex;
+QT_END_NAMESPACE
+
+/** Masternode Manager page widget */
+class OverviewPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit OverviewPage(const PlatformStyle* platformStyle, QWidget* parent = 0);
+    ~OverviewPage();
+
+    void setClientModel(ClientModel* clientModel);
+    void setWalletModel(WalletModel* walletModel);
+
+private:
+    QMenu* contextMenuDIP3;
+    int64_t nTimeFilterUpdatedDIP3;
+    int64_t nTimeUpdatedDIP3;
+    bool fFilterUpdatedDIP3;
+
+    QTimer* timer;
+    Ui::OverviewPage* ui;
+    ClientModel* clientModel;
+    WalletModel* walletModel;
+
+    // Protects tableWidgetMasternodesDIP3
+    CCriticalSection cs_dip3list;
+
+    QString strCurrentFilterDIP3;
+
+    bool mnListChanged;
+
+    CDeterministicMNCPtr GetSelectedDIP3MN();
+
+    void updateDIP3List();
+
+Q_SIGNALS:
+    void doubleClicked(const QModelIndex&);
+
+private Q_SLOTS:
+    void showContextMenuDIP3(const QPoint&);
+    void on_filterLineEditDIP3_textChanged(const QString& strFilterIn);
+    void on_checkBoxMyMasternodesOnly_stateChanged(int state);
+
+    void extraInfoDIP3_clicked();
+    void copyProTxHash_clicked();
+    void copyCollateralOutpoint_clicked();
+
+    void handleMasternodeListChanged();
+    void updateDIP3ListScheduled();
+};
+
+
 #endif // BITCOIN_QT_OVERVIEWPAGE_H
