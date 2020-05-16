73384a7eda2897417ef785daca624881e91adda9
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-16 13:56:39 -0400

    Update walletframe.h

--- src/qt/walletframe.h
+++ src/qt/walletframe.h
@@ -63,10 +63,6 @@ private:
 
 public Q_SLOTS:
 
-
-    /** Switch to Proposal Add Page */
-	void gotoProposalAddPage();
-	
     /** Switch to governance page */
     void gotoGovernancePage(); 
     /** Switch to private send page */
