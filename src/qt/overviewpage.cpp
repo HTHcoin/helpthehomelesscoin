4046e8a365dbf0ce065f7193cc30e6b29f39248b
Devilking6105 <devilking6105@gmail.com>
2020-05-17 13:21:39 -0400

    Revert QT

---
+++ src/qt/overviewpage.cpp
@@ -0,0 +1,314 @@
+// Copyright (c) 2011-2015 The Bitcoin Core developers
+// Copyright (c) 2014-2018 The Dash Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "overviewpage.h"
+#include "ui_overviewpage.h"
+
+#include "activemasternode.h"
+#include "clientversion.h"
+#include "netbase.h"
+#include "sync.h"
+#include "walletmodel.h"
+#include "evo/deterministicmns.h"
+
+#include "bitcoinunits.h"
+#include "clientmodel.h"
+#include "guiconstants.h"
+#include "guiutil.h"
+#include "init.h"
+#include "optionsmodel.h"
+#include "platformstyle.h"
+#include "transactionfilterproxy.h"
+#include "transactiontablemodel.h"
+#include "utilitydialog.h"
+#include "walletmodel.h"
+#include "rpc/blockchain.cpp"
+#include "chainparams.h"
+#include "amount.h"
+#include "validation.h"
+#include "wallet/wallet.h"
+#include "net.h"
+
+#include "instantx.h"
+#include "masternode-sync.h"
+
+
+#include <univalue.h>
+
+#include <QMessageBox>
+#include <QtGui/QClipboard>
+#include <QAbstractItemDelegate>
+#include <QPainter>
+#include <QSettings>
+#include <QTimer>
+#include <QUrl>
+#include <QDesktopServices>
+#include <QtNetwork/QNetworkAccessManager>	
+#include <QtNetwork/QNetworkReply>
+
+#define ICON_OFFSET 16
+#define DECORATION_SIZE 54
+#define NUM_ITEMS 5
+#define NUM_ITEMS_ADV 7
+
+#include "overviewpage.moc"
+
+OverviewPage::OverviewPage(const PlatformStyle *platformStyle, QWidget *parent) :
+  
+    QWidget(parent),
+    timer(nullptr),
+    ui(new Ui::OverviewPage),
+    clientModel(0),
+    walletModel(0),
+    currentBalance(-1),
+    currentUnconfirmedBalance(-1),
+    currentImmatureBalance(-1),
+    currentWatchOnlyBalance(-1),
+    currentWatchUnconfBalance(-1),
+    currentWatchImmatureBalance(-1),
+    cachedNumISLocks(-1)
+    
+{
+               
+    ui->setupUi(this);
+    QString theme = GUIUtil::getThemeName();
+
+     
+    ui->pushButton_Website->setStatusTip(tr("Visit Help The Homeless Worldwide A NJ Nonprofit Corporation"));
+    ui->pushButton_Website_1->setStatusTip(tr("Visit Help The Homeless Coin"));
+    ui->pushButton_Website_2->setStatusTip(tr("Visit AltMarkets.io to trade Help The Homeless Coin"));
+    ui->pushButton_Website_3->setStatusTip(tr("Visit Open Chainz to see the Help The Homeless Coin Explorer"));  
+    ui->pushButton_Website_4->setStatusTip(tr("Visit Help The Homeless Worldwide A NJ Nonprofit Corporation Partners"));
+    ui->pushButton_Website_5->setStatusTip(tr("Visit AltMarkets.io to trade Help The Homeless Coin"));  
+        
+    // init "out of sync" warning labels
+    ui->labelWalletStatus->setText("(" + tr("out of sync") + ")");
+  
+    //information block update
+   
+      
+    timerinfo_mn = new QTimer(this);
+    connect(timerinfo_mn, SIGNAL(timeout()), this, SLOT(updateMasternodeInfo()));
+    timerinfo_mn->start(1000);  
+      
+    timerinfo_blockchain = new QTimer(this);
+    connect(timerinfo_blockchain, SIGNAL(timeout()), this, SLOT(updateBlockChainInfo()));
+    timerinfo_blockchain->start(1000); //30sec    
+      
+    timerinfo_peers = new QTimer(this);
+    connect(timerinfo_peers, SIGNAL(timeout()), this, SLOT(updatePeersInfo()));
+    timerinfo_peers->start(1000); 
+      
+                  
+    // start with displaying the "out of sync" warnings
+    showOutOfSyncWarning(true);
+
+    // that's it for litemode
+    if(fLiteMode) return;
+}
+
+void OverviewPage::handleOutOfSyncWarningClicks()
+{
+    Q_EMIT outOfSyncWarningClicked();
+}
+
+OverviewPage::~OverviewPage()
+{
+    /*if(timer) disconnect(timer, SIGNAL(timeout()), this, SLOT(privateSendStatus()));
+    delete ui; */
+  
+  delete ui;
+}
+
+void OverviewPage::setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance, const CAmount& anonymizedBalance, const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance)
+{
+    currentBalance = balance;
+    currentUnconfirmedBalance = unconfirmedBalance;
+    currentImmatureBalance = immatureBalance;
+    currentAnonymizedBalance = anonymizedBalance;
+    currentWatchOnlyBalance = watchOnlyBalance;
+    currentWatchUnconfBalance = watchUnconfBalance;
+    currentWatchImmatureBalance = watchImmatureBalance;
+    ui->labelBalance->setText(BitcoinUnits::floorHtmlWithUnit(nDisplayUnit, balance, false, BitcoinUnits::separatorAlways));
+    ui->labelUnconfirmed->setText(BitcoinUnits::floorHtmlWithUnit(nDisplayUnit, unconfirmedBalance, false, BitcoinUnits::separatorAlways));
+    ui->labelImmature->setText(BitcoinUnits::floorHtmlWithUnit(nDisplayUnit, immatureBalance, false, BitcoinUnits::separatorAlways));
+    ui->labelUnconfirmed->setText(BitcoinUnits::floorHtmlWithUnit(nDisplayUnit, anonymizedBalance, false, BitcoinUnits::separatorAlways));
+    ui->labelTotal->setText(BitcoinUnits::floorHtmlWithUnit(nDisplayUnit, balance + unconfirmedBalance + immatureBalance, false, BitcoinUnits::separatorAlways));
+    ui->labelWatchAvailable->setText(BitcoinUnits::floorHtmlWithUnit(nDisplayUnit, watchOnlyBalance, false, BitcoinUnits::separatorAlways));
+    ui->labelWatchPending->setText(BitcoinUnits::floorHtmlWithUnit(nDisplayUnit, watchUnconfBalance, false, BitcoinUnits::separatorAlways));
+    ui->labelWatchImmature->setText(BitcoinUnits::floorHtmlWithUnit(nDisplayUnit, watchImmatureBalance, false, BitcoinUnits::separatorAlways));
+    ui->labelWatchTotal->setText(BitcoinUnits::floorHtmlWithUnit(nDisplayUnit, watchOnlyBalance + watchUnconfBalance + watchImmatureBalance, false, BitcoinUnits::separatorAlways));
+    
+    // only show immature (newly mined) balance if it's non-zero, so as not to complicate things
+    // for the non-mining users
+    bool showImmature = immatureBalance != 0;
+    bool showWatchOnlyImmature = watchImmatureBalance != 0;
+
+    // for symmetry reasons also show immature label when the watch-only one is shown
+    ui->labelImmature->setVisible(showImmature || showWatchOnlyImmature);
+    ui->labelImmatureText->setVisible(showImmature || showWatchOnlyImmature);
+    ui->labelWatchImmature->setVisible(showWatchOnlyImmature); // show watch-only immature balance
+}
+
+// show/hide watch-only labels
+void OverviewPage::updateWatchOnlyLabels(bool showWatchOnly)
+{
+    ui->labelSpendable->setVisible(showWatchOnly);      // show spendable label (only when watch-only is active)
+    ui->labelWatchonly->setVisible(showWatchOnly);      // show watch-only label
+    ui->lineWatchBalance->setVisible(showWatchOnly);    // show watch-only balance separator line
+    ui->labelWatchAvailable->setVisible(showWatchOnly); // show watch-only available balance
+    ui->labelWatchPending->setVisible(showWatchOnly);   // show watch-only pending balance
+    ui->labelWatchTotal->setVisible(showWatchOnly);     // show watch-only total balance
+
+    if (!showWatchOnly){
+        ui->labelWatchImmature->hide();
+    }
+    else{
+        ui->labelBalance->setIndent(20);
+        ui->labelUnconfirmed->setIndent(20);
+        ui->labelImmatureText->setIndent(20);
+        ui->labelTotal->setIndent(20);
+    }
+}
+
+void OverviewPage::setClientModel(ClientModel *model)
+{
+    this->clientModel = model;
+    if(model)
+    {
+        // Show warning if this is a prerelease version
+       /* connect(model, SIGNAL(alertsChanged(QString)), this, SLOT(updateAlerts(QString)));
+         updateAlerts(model->getStatusBarWarnings()); */
+    }
+}
+
+void OverviewPage::setWalletModel(WalletModel *model)
+{
+    this->walletModel = model;
+    if(model && model->getOptionsModel())
+    {
+        // update the display unit, to not use the default ("HTH")
+        updateDisplayUnit();
+        // Keep up to date with wallet
+        setBalance(model->getBalance(), model->getUnconfirmedBalance(), model->getImmatureBalance(), model->getAnonymizedBalance(),
+                   model->getWatchBalance(), model->getWatchUnconfirmedBalance(), model->getWatchImmatureBalance());
+        connect(model, SIGNAL(balanceChanged(CAmount,CAmount,CAmount,CAmount,CAmount,CAmount,CAmount)), this, SLOT(setBalance(CAmount,CAmount,CAmount,CAmount,CAmount,CAmount,CAmount)));
+
+        connect(model->getOptionsModel(), SIGNAL(displayUnitChanged(int)), this, SLOT(updateDisplayUnit()));
+        updateWatchOnlyLabels(model->haveWatchOnly());
+        connect(model, SIGNAL(notifyWatchonlyChanged(bool)), this, SLOT(updateWatchOnlyLabels(bool)));    
+    }
+}
+
+
+
+void OverviewPage::updateDisplayUnit()
+{
+    if(walletModel && walletModel->getOptionsModel())
+    {
+        nDisplayUnit = walletModel->getOptionsModel()->getDisplayUnit();
+        if(currentBalance != -1)
+            setBalance(currentBalance, currentUnconfirmedBalance, currentImmatureBalance, currentAnonymizedBalance,
+                       currentWatchOnlyBalance, currentWatchUnconfBalance, currentWatchImmatureBalance);
+
+    }
+}
+
+/**** Blockchain Information *****/
+
+
+ void OverviewPage::updateMasternodeInfo()
+{
+    if (!clientModel) {
+        return;
+    }
+    auto mnList = clientModel->getMasternodeList();
+    QString strMasternodeCount = tr("%1")
+        .arg(QString::number(mnList.GetAllMNsCount()))
+     /*   .arg(QString::number(mnList.GetValidMNsCount()))*/;
+    ui->countLabelDIP3->setText(strMasternodeCount);
+}
+
+
+ void OverviewPage::updatePeersInfo()  /** Peer Info  **/
+{
+  if (masternodeSync.IsBlockchainSynced() && masternodeSync.IsSynced())
+   
+  {
+    
+          (timerinfo_peers->interval() == 1000);
+           timerinfo_peers->setInterval(180000);
+           
+           int PeerCount = clientModel->getNumConnections();
+           ui->label_count_2->setText(QString::number(PeerCount));
+  }
+}
+
+void OverviewPage::updateBlockChainInfo()
+{
+    if (masternodeSync.IsBlockchainSynced())
+    {
+        int CurrentBlock = clientModel->getNumBlocks();
+        double CurrentDiff = GetDifficulty();
+      
+        ui->label_CurrentBlock_value_3->setText(QString::number(CurrentBlock));
+        ui->label_Nethash_3->setText(tr("Difficulty:"));
+        ui->label_Nethash_value_3->setText(QString::number(CurrentDiff,'f',4));    
+    }
+}
+
+                /**** End Blockchain Information ******/
+
+
+
+void OverviewPage::showOutOfSyncWarning(bool fShow)
+{
+    ui->labelWalletStatus->setVisible(fShow);  
+
+ /*   ui->labelTransactionsStatus->setVisible(fShow);  */
+}
+
+/************** HTH Worldwide Button ******************/
+ 
+void OverviewPage::on_pushButton_Website_clicked() {  // Nonprofit Wesbite
+    
+    QDesktopServices::openUrl(QUrl("https://www.helpthehomelessworldwide.org/", QUrl::TolerantMode));
+    
+}
+
+void OverviewPage::on_pushButton_Website_1_clicked() {  // HTH Coin Wesbite
+    
+    QDesktopServices::openUrl(QUrl("https://hth.world", QUrl::TolerantMode));
+    
+}
+
+
+void OverviewPage::on_pushButton_Website_2_clicked() {  // HTH Exchanges
+    
+    QDesktopServices::openUrl(QUrl("https://altmarkets.io/trading/hthbtc", QUrl::TolerantMode));
+    
+}
+
+void OverviewPage::on_pushButton_Website_3_clicked() {  // HTH Explorer
+    
+    QDesktopServices::openUrl(QUrl("https://chainz.cryptoid.info/hth", QUrl::TolerantMode));
+    
+}
+
+void OverviewPage::on_pushButton_Website_4_clicked() {  // HTH Partners
+    
+    QDesktopServices::openUrl(QUrl("https://hth.world/partners.html", QUrl::TolerantMode));
+    
+}
+
+
+void OverviewPage::on_pushButton_Website_5_clicked() {  // HTH Partners
+    
+    QDesktopServices::openUrl(QUrl("https://nortexchange.com/exchange/?market=HTH_BTC", QUrl::TolerantMode));
+    
+}
+
+
+/************** HTH Worldwide Button *****************/
---
+++ src/qt/walletmodeltransaction.cpp
@@ -0,0 +1,106 @@
+// Copyright (c) 2011-2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "walletmodeltransaction.h"
+
+#include "wallet/wallet.h"
+
+WalletModelTransaction::WalletModelTransaction(const QList<SendCoinsRecipient> &_recipients) :
+    recipients(_recipients),
+    walletTransaction(0),
+    keyChange(0),
+    fee(0)
+{
+    walletTransaction = new CWalletTx();
+}
+
+WalletModelTransaction::~WalletModelTransaction()
+{
+    delete keyChange;
+    delete walletTransaction;
+}
+
+QList<SendCoinsRecipient> WalletModelTransaction::getRecipients()
+{
+    return recipients;
+}
+
+CWalletTx *WalletModelTransaction::getTransaction()
+{
+    return walletTransaction;
+}
+
+unsigned int WalletModelTransaction::getTransactionSize()
+{
+    return (!walletTransaction ? 0 : (::GetSerializeSize(walletTransaction->tx, SER_NETWORK, PROTOCOL_VERSION)));
+}
+
+CAmount WalletModelTransaction::getTransactionFee()
+{
+    return fee;
+}
+
+void WalletModelTransaction::setTransactionFee(const CAmount& newFee)
+{
+    fee = newFee;
+}
+
+void WalletModelTransaction::reassignAmounts()
+{
+    // For each recipient look for a matching CTxOut in walletTransaction and reassign amounts
+    for (QList<SendCoinsRecipient>::iterator it = recipients.begin(); it != recipients.end(); ++it)
+    {
+        SendCoinsRecipient& rcp = (*it);
+
+        if (rcp.paymentRequest.IsInitialized())
+        {
+            CAmount subtotal = 0;
+            const payments::PaymentDetails& details = rcp.paymentRequest.getDetails();
+            for (int j = 0; j < details.outputs_size(); j++)
+            {
+                const payments::Output& out = details.outputs(j);
+                if (out.amount() <= 0) continue;
+                const unsigned char* scriptStr = (const unsigned char*)out.script().data();
+                CScript scriptPubKey(scriptStr, scriptStr+out.script().size());
+                for (const auto& txout : walletTransaction->tx->vout) {
+                    if (txout.scriptPubKey == scriptPubKey) {
+                        subtotal += txout.nValue;
+                        break;
+                    }
+                }
+            }
+            rcp.amount = subtotal;
+        }
+        else // normal recipient (no payment request)
+        {
+            for (const auto& txout : walletTransaction->tx->vout) {
+                CScript scriptPubKey = GetScriptForDestination(CBitcoinAddress(rcp.address.toStdString()).Get());
+                if (txout.scriptPubKey == scriptPubKey) {
+                    rcp.amount = txout.nValue;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+CAmount WalletModelTransaction::getTotalTransactionAmount()
+{
+    CAmount totalTransactionAmount = 0;
+    Q_FOREACH(const SendCoinsRecipient &rcp, recipients)
+    {
+        totalTransactionAmount += rcp.amount;
+    }
+    return totalTransactionAmount;
+}
+
+void WalletModelTransaction::newPossibleKeyChange(CWallet *wallet)
+{
+    keyChange = new CReserveKey(wallet);
+}
+
+CReserveKey *WalletModelTransaction::getPossibleKeyChange()
+{
+    return keyChange;
+}
