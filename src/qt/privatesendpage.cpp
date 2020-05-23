2425a9a07442f2b100db6f09b380d518c77452c5
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-22 20:59:59 -0400

    Update privatesendpage.cpp

--- src/qt/privatesendpage.cpp
+++ src/qt/privatesendpage.cpp
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "privatesendpage.h"
-#include "ui_privatepage.h"
+#include "ui_privatesendpage.h"
 
 
 #include "bitcoinunits.h"
