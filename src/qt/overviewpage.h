1a56e56bdcade38d0737eb2407d482b0224e0051
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-28 18:43:53 -0400

    Update overviewpage.h

--- src/qt/overviewpage.h
+++ src/qt/overviewpage.h
@@ -132,7 +132,7 @@ private Q_SLOTS:
     void updateBlockChainInfo();
     void updateMasternodeInfo(); 
     void updatePeersInfo();
-    void getPriceInfo();
+    void updatePriceInfo();
  };
 
 #endif // BITCOIN_QT_OVERVIEWPAGE_H
