a15eb97fd39f2b4c08a116414dfe379a4afb90ab
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-28 20:04:37 -0400

    Update clientmodel.cpp

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -240,7 +240,7 @@ enum BlockSource ClientModel::getBlockSource() const
     return BLOCK_SOURCE_NONE;
 }
 
-void ClientModel::getPriceInfo()
+float ClientModel::getPriceInfo()
 {
    
    request->setUrl(QUrl("https://api.binance.com/api/v1/ticker/price?symbol=RVNBTC"));
