abb6bfc55192bab5342d0064a096b005734392e8
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-05-29 21:25:59 -0400

    Create proposalrecord.cpp

---
+++ src/qt/proposalrecord.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2017-2018 The Bulwark Developers
+// Copyright (c) 2018 The Curium developers
+// Copyright (c) 2018-2018 The Phore developers
+// Copyright (c) 2019 The ODIN developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "proposalrecord.h"
