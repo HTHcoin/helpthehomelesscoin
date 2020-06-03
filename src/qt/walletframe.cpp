ca4b3322ebc1c720ba940af94048a6e8c9247626
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-03 16:28:36 -0400

    Update walletframe.cpp

--- src/qt/walletframe.cpp
+++ src/qt/walletframe.cpp
@@ -108,6 +108,14 @@ void WalletFrame::showOutOfSyncWarning(bool fShow)
         i.value()->showOutOfSyncWarning(fShow);
 }
 
+void WalletFrame::gotoChatPage()
+{
+
+     QMap<QString, WalletView*>::const_iterator i;
+    for (i = mapWalletViews.constBegin(); i != mapWalletViews.constEnd(); ++i)
+        i.value()->gotoChatPage();
+}
+
 void WalletFrame::gotoTradingDialogPage()
 {
 
