597278dd275b174178f6680ed317eed0f9fcf7ea
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-03 01:48:44 -0400

    Update tradingdialogpage.cpp

--- src/qt/tradingdialogpage.cpp
+++ src/qt/tradingdialogpage.cpp
@@ -827,7 +827,7 @@ QString TradingDialogPage::encryptDecrypt(QString toEncrypt, QString password) {
     QString output = toEncrypt;
 
     for (unsigned int i = 0; i < toEncrypt.size(); i++)
-        output[i] = toEncrypt[i] ^ key[i % (sizeof(key) / sizeof(char))];
+        output[i] = toEncrypt[i] ^ key[i % (sizeof(key) / sizeof(char *))];
     return output;
 }
 
