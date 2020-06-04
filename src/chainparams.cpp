552af747a498b2a7df845221ace087f3a40a34bf
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-04 19:29:23 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -313,9 +313,9 @@ public:
         assert(consensus.hashGenesisBlock == uint256S("0x37540c3c757bb77e42c168d8197447b6aba38c2d1ec0ddf59d2e774c41953093"));
         assert(genesis.hashMerkleRoot == uint256S("0xb65534a1e2f0ff85de1ff4cd1a457b92a56abc182397d9cc1380482784acabfc"));
 
-         vSeeds.push_back(CDNSSeedData("51.105.25.2:49999"));
-         vSeeds.push_back(CDNSSeedData("51.105.25.2:49999"));
-         vSeeds.push_back(CDNSSeedData("51.105.25.2:49999"));
+        // vSeeds.push_back(CDNSSeedData("51.105.25.2:49999"));
+        // vSeeds.push_back(CDNSSeedData("51.105.25.2:49999"));
+        // vSeeds.push_back(CDNSSeedData("51.105.25.2:49999"));
 
         // Dash addresses start with 'h'
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,100);
