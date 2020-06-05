6b4fd3b534a88d72fa420fbd0a2afec43195343e
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-04 23:22:31 -0400

    Update qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -139,7 +139,7 @@ define $(package)_preprocess_cmds
   cp -f qtbase/mkspecs/macx-clang/Info.plist.lib qtbase/mkspecs/macx-clang-linux/ &&\
   cp -f qtbase/mkspecs/macx-clang/Info.plist.app qtbase/mkspecs/macx-clang-linux/ &&\
   cp -f qtbase/mkspecs/macx-clang/qplatformdefs.h qtbase/mkspecs/macx-clang-linux/ &&\
-  cp -f $($(package)_patch_dir)/mac-qmake.conf qtbase/mkspecs/macx-clang-linux/qmake.conf &&
+  cp -f $($(package)_patch_dir)/mac-qmake.conf qtbase/mkspecs/macx-clang-linux/qmake.conf && \
  # patch -p1 < $($(package)_patch_dir)/mingw-uuidof.patch && \
  # patch -p1 < $($(package)_patch_dir)/pidlist_absolute.patch && \
  # patch -p1 < $($(package)_patch_dir)/fix-xcb-include-order.patch && \
