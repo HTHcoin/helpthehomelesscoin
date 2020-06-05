5fb59a595f5222c16ab633487215f942925fef88
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-04 21:00:02 -0400

    Update chatwindowpage.h

--- src/qt/chatwindowpage.h
+++ src/qt/chatwindowpage.h
@@ -1,73 +1,79 @@
-/*Copyright (C) 2009 Cleriot Simon
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA*/
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
 
 #ifndef CHATWINDOWPAGE_H
 #define CHATWINDOWPAGE_H
 
-#include <QtGui>
-#include <QtNetwork>
-#include "clientmodel.h"
-#include "serveur.h"
+#include "ui_chatwindowpage.h"
+#include "client.h"
 
-#include <QDesktopServices>
-#include <QProcess>
-#include <QDir>
-
-
-
-namespace Ui
-{
-    class ChatWindowPage;
-}
-
-class ChatWindowPage : public QWidget
+class ChatWindowPage : public QDialog, private Ui::ChatWindowPage
 {
     Q_OBJECT
 
 public:
-    ChatWindowPage(QWidget *parent = 0);
-    ~ChatWindowPage();
-    void setModel(ClientModel *model);
-    Serveur * currentTab();
-	Q_SIGNALS:
-		void changeTab();
+    ChatWindowPage(QWidget *parent = nullptr);
 
-	public Q_SLOTS:
-		void sendCommande();
-        void connecte();
-		void closeTab();
+public Q_SLOTS:
+    void appendMessage(const QString &from, const QString &message);
 
-		void tabChanged(int index);
-
-		void tabJoined();
-		void tabJoining();
-        void disconnectFromServer();
-        void tabClosing(int index);
-
+private Q_SLOTS:
+    void returnPressed();
+    void newParticipant(const QString &nick);
+    void participantLeft(const QString &nick);
+    void showInformation();
 
 private:
-	Ui::ChatWindowPage *ui;
-    ClientModel *model;
-    QMap<QString,Serveur *> serveurs;
-	bool joining;
-	void closeEvent(QCloseEvent *event);
-	
-private Q_SLOTS:	
-	void on_pushButton_WebChat_clicked();
-
+    Client client;
+    QString myNickName;
+    QTextTableFormat tableFormat;
 };
 
-#endif // CHATWINDOWPAGE_H
+#endif
