72419658d7d9f07be5cb241e351b56ebe2c2d1a6
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-08 14:21:03 -0400

    Update chatwindowpage.h

--- src/qt/chatwindowpage.h
+++ src/qt/chatwindowpage.h
@@ -1,79 +1,26 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
 #ifndef CHATWINDOWPAGE_H
 #define CHATWINDOWPAGE_H
 
-#include "ui_chatwindowpage.h"
-#include "client.h"
-
-class ChatWindowPage : public QDialog, private Ui::ChatWindowPage
+#include <QMainWindow>
+#include"servedev.h"
+namespace Ui {
+class ChatWindowPage;
+}
+class servedev;
+class ChatWindowPage : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    ChatWindowPage(QWidget *parent = nullptr);
-
-public Q_SLOTS:
-    void appendMessage(const QString &from, const QString &message);
-
+    explicit ChatWindowPage(QWidget *parent = 0);
+    ~ChatWindowPage();
+    void addMessage(QString Msg);
+    servedev* m_pBoxServer;
 private Q_SLOTS:
-    void returnPressed();
-    void newParticipant(const QString &nick);
-    void participantLeft(const QString &nick);
-    void showInformation();
+    void on_pushButtonStart_clicked();
 
 private:
-    Client client;
-    QString myNickName;
-    QTextTableFormat tableFormat;
+    Ui::ChatWindowPage *ui;
 };
 
-#endif
+#endif // CHATWINDOWPAGE_H
