6b565fae307c35dfee3bd2302b2fffbc34182d6d
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-08 16:44:11 -0400

    Create chatwindowpage.cpp

---
+++ src/qt/chatwindowpage.cpp
@@ -0,0 +1,113 @@
+#include "ChatWindowPage.h"
+
+// We'll need some regular expression magic in this code:
+#include <QRegExp>
+
+// This is our ChatWindowPage constructor (you C++ n00b)
+ChatWindowPage::ChatWindowPage(QWidget *parent) : QMainWindow(parent)
+{
+    // When using Designer, you should always call setupUi(this)
+    // in your constructor. This creates and lays out all the widgets
+    // on the ChatWindowPage that you setup in Designer.
+    setupUi(this);
+
+    // Make sure that we are showing the login page when we startup:
+    stackedWidget->setCurrentWidget(loginPage);
+
+    // Instantiate our socket (but don't actually connect to anything
+    // yet until the user clicks the loginButton:
+    socket = new QTcpSocket(this);
+
+    // This is how we tell Qt to call our readyRead() and connected()
+    // functions when the socket has text ready to be read, and is done
+    // connecting to the server (respectively):
+    connect(socket, SIGNAL(readyRead()), this, SLOT(readyRead()));
+    connect(socket, SIGNAL(connected()), this, SLOT(connected()));
+}
+
+// This gets called when the loginButton gets clicked:
+// We didn't have to use connect() to set this up because
+// Qt recognizes the name of this function and knows to set
+// up the signal/slot connection for us.
+void ChatWindowPage::on_loginButton_clicked()
+{
+    // Start connecting to the chat server (on port 4200).
+    // This returns immediately and then works on connecting
+    // to the server in the background. When it's done, we'll
+    // get a connected() function call (below). If it fails,
+    // we won't get any error message because we didn't connect()
+    // to the error() signal from this socket.
+    socket->connectToHost(serverLineEdit->text(), 4200);
+}
+
+// This gets called when the user clicks the sayButton (next to where
+// they type text to send to the chat room):
+void ChatWindowPage::on_sayButton_clicked()
+{
+    // What did they want to say (minus white space around the string):
+    QString message = sayLineEdit->text().trimmed();
+
+    // Only send the text to the chat server if it's not empty:
+    if(!message.isEmpty())
+    {
+        socket->write(QString(message + "\n").toUtf8());
+    }
+
+    // Clear out the input box so they can type something else:
+    sayLineEdit->clear();
+
+    // Put the focus back into the input box so they can type again:
+    sayLineEdit->setFocus();
+}
+
+// This function gets called whenever the chat server has sent us some text:
+void ChatWindowPage::readyRead()
+{
+    // We'll loop over every (complete) line of text that the server has sent us:
+    while(socket->canReadLine())
+    {
+        // Here's the line the of text the server sent us (we use UTF-8 so
+        // that non-English speakers can chat in their native language)
+        QString line = QString::fromUtf8(socket->readLine()).trimmed();
+
+        // These two regular expressions describe the kinds of messages
+        // the server can send us:
+
+        //  Normal messges look like this: "username:The message"
+        QRegExp messageRegex("^([^:]+):(.*)$");
+
+        // Any message that starts with "/users:" is the server sending us a
+        // list of users so we can show that list in our GUI:
+        QRegExp usersRegex("^/users:(.*)$");
+
+        // Is this a users message:
+        if(usersRegex.indexIn(line) != -1)
+        {
+            // If so, udpate our users list on the right:
+            QStringList users = usersRegex.cap(1).split(",");
+            userListWidget->clear();
+            foreach(QString user, users)
+                new QListWidgetItem(QPixmap(":/user.png"), user, userListWidget);
+        }
+        // Is this a normal chat message:
+        else if(messageRegex.indexIn(line) != -1)
+        {
+            // If so, append this message to our chat box:
+            QString user = messageRegex.cap(1);
+            QString message = messageRegex.cap(2);
+
+            roomTextEdit->append("<b>" + user + "</b>: " + message);
+        }
+    }
+}
+
+// This function gets called when our socket has successfully connected to the chat
+// server. (see the connect() call in the ChatWindowPage constructor).
+void ChatWindowPage::connected()
+{
+    // Flip over to the chat page:
+    stackedWidget->setCurrentWidget(chatPage);
+
+    // And send our username to the chat server.
+    socket->write(QString("/me:" + userLineEdit->text() + "\n").toUtf8());
+}
