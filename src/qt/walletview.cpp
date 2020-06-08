01543cd784dac9b08a567f4465bc567d53e45bf4
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-08 19:49:13 -0400

    Update walletview.cpp

--- src/qt/walletview.cpp
+++ src/qt/walletview.cpp
@@ -88,7 +88,7 @@ WalletView::WalletView(const PlatformStyle *_platformStyle, QWidget *parent):
    /* tradingDialogPage = new TradingDialogPage();
     addWidget(tradingDialogPage); */
 	    
-    chatMainWindow = new MainWindow();
+    chatMainWindow = new chatMainWindow();
     addWidget(chatMainWindow);	    
 
     QSettings settings;
