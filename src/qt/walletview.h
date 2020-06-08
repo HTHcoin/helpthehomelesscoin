9b0ae020ca6754cdd2d934db9815f1f9622327a1
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-08 19:56:08 -0400

    Update walletview.h

--- src/qt/walletview.h
+++ src/qt/walletview.h
@@ -10,7 +10,7 @@
 #include "governancelist.h"
 /* #include "tradingdialogpage.h" */ 
 #include "chatwindowpage.h" 
-#include "qt/client/mainwindow.h"
+#include "client/mainwindow.h"
 
 
 #include <QStackedWidget>
