d63ad2769a69d0bd96020c050cb72035b8d1e192
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-08 14:22:50 -0400

    Update chatwindowpage.cpp

--- src/qt/chatwindowpage.cpp
+++ src/qt/chatwindowpage.cpp
@@ -1,151 +1,40 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
-#include <QtWidgets>
-
 #include "chatwindowpage.h"
-
-ChatWindowPage::ChatWindowPage(QWidget *parent)
-    : QDialog(parent)
+#include "ui_chatwindowpage.h"
+#include"servedev.h"
+ChatWindowPage::ChatWindowPage(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::ChatWindowPage)
 {
-    setupUi(this);
+    ui->setupUi(this);
+    //m_pBoxServer = new servedev(this);
 
-    lineEdit->setFocusPolicy(Qt::StrongFocus);
-    textEdit->setFocusPolicy(Qt::NoFocus);
-    textEdit->setReadOnly(true);
-    listWidget->setFocusPolicy(Qt::NoFocus);
-
-    connect(lineEdit, &QLineEdit::returnPressed,
-            this, &ChatWindowPage::returnPressed);
-    connect(&client, &Client::newMessage,
-            this, &ChatWindowPage::appendMessage);
-    connect(&client, &Client::newParticipant,
-            this, &ChatWindowPage::newParticipant);
-    connect(&client, &Client::participantLeft,
-            this, &ChatWindowPage::participantLeft);
-
-    myNickName = client.nickName();
-    newParticipant(myNickName);
-    tableFormat.setBorder(0);
-    QTimer::singleShot(10 * 1000, this, SLOT(showInformation()));
 }
 
-void ChatWindowPage::appendMessage(const QString &from, const QString &message)
+ChatWindowPage::~ChatWindowPage()
 {
-    if (from.isEmpty() || message.isEmpty())
-        return;
-
-    QTextCursor cursor(textEdit->textCursor());
-    cursor.movePosition(QTextCursor::End);
-    QTextTable *table = cursor.insertTable(1, 2, tableFormat);
-    table->cellAt(0, 0).firstCursorPosition().insertText('<' + from + "> ");
-    table->cellAt(0, 1).firstCursorPosition().insertText(message);
-    QScrollBar *bar = textEdit->verticalScrollBar();
-    bar->setValue(bar->maximum());
+    delete ui;
 }
 
-void ChatWindowPage::returnPressed()
+void ChatWindowPage::addMessage(QString Msg)
 {
-    QString text = lineEdit->text();
-    if (text.isEmpty())
-        return;
 
-    if (text.startsWith(QChar('/'))) {
-        QColor color = textEdit->textColor();
-        textEdit->setTextColor(Qt::red);
-        textEdit->append(tr("! Unknown command: %1")
-                         .arg(text.left(text.indexOf(' '))));
-        textEdit->setTextColor(color);
-    } else {
-        client.sendMessage(text);
-        appendMessage(myNickName, text);
+    ui->textEditStatus->setText(Msg);
+
+}
+
+void ChatWindowPage::on_pushButtonStart_clicked()
+{
+    m_pBoxServer = new servedev(this);
+
+    bool success = m_pBoxServer->listen(QHostAddress::Any, quint16(ui->textEditPort->toPlainText().toInt()));
+    if(!success)
+    {
+        addMessage("Server failed...");
+
+    }
+    else
+    {
+        addMessage("Server Started...");
     }
 
-    lineEdit->clear();
-}
-
-void ChatWindowPage::newParticipant(const QString &nick)
-{
-    if (nick.isEmpty())
-        return;
-
-    QColor color = textEdit->textColor();
-    textEdit->setTextColor(Qt::gray);
-    textEdit->append(tr("* %1 has joined").arg(nick));
-    textEdit->setTextColor(color);
-    listWidget->addItem(nick);
-}
-
-void ChatWindowPage::participantLeft(const QString &nick)
-{
-    if (nick.isEmpty())
-        return;
-
-    QList<QListWidgetItem *> items = listWidget->findItems(nick,
-                                                           Qt::MatchExactly);
-    if (items.isEmpty())
-        return;
-
-    delete items.at(0);
-    QColor color = textEdit->textColor();
-    textEdit->setTextColor(Qt::gray);
-    textEdit->append(tr("* %1 has left").arg(nick));
-    textEdit->setTextColor(color);
-}
-
-void ChatWindowPage::showInformation()
-{
-    if (listWidget->count() == 1) {
-        QMessageBox::information(this, tr("Chat"),
-                                 tr("Launch several instances of this "
-                                    "program on your local network and "
-                                    "start chatting!"));
-    }
 }
