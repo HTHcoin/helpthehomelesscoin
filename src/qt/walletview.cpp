ff17bf81d43c62e2fede3fbfde6ba0095c3e044d
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-09 19:50:34 -0400

    Update walletview.cpp

--- src/qt/walletview.cpp
+++ src/qt/walletview.cpp
@@ -88,8 +88,8 @@ WalletView::WalletView(const PlatformStyle *_platformStyle, QWidget *parent):
    /* tradingDialogPage = new TradingDialogPage();
     addWidget(tradingDialogPage); */
 	    
-    chatWindowPage = new ChatWindowPage();
-    addWidget(chatWindowPage);	    
+    loginPage = new LoginPage();
+    addWidget(loginPage);	    
 
     QSettings settings;
     if (!fLiteMode && settings.value("fShowMasternodesTab").toBool()) {
@@ -241,9 +241,9 @@ void WalletView::processNewTransaction(const QModelIndex& parent, int start, int
     Q_EMIT incomingTransaction(date, walletModel->getOptionsModel()->getDisplayUnit(), amount, type, address, label);
 }
 
-void WalletView::gotoChatWindowPage()
+void WalletView::gotoLoginPage()
 {
-   setCurrentWidget(chatWindowPage);
+   setCurrentWidget(loginPage);
 } 
 
 /*void WalletView::gotoTradingDialogPage()
