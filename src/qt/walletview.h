0a62816e12e8b6ef07f605b368b4d146021edcbb
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-10 16:24:41 -0400

    Update walletview.h

--- src/qt/walletview.h
+++ src/qt/walletview.h
@@ -9,7 +9,7 @@
 #include "masternodelist.h"
 #include "governancelist.h"
 /* #include "tradingdialogpage.h" */ 
-#include "mainwindow.h" 
+#include "newaccount.h" 
 
 
 
@@ -28,7 +28,7 @@ class AddressBookPage;
 class PrivateSendPage;
 class GovernancePage;
 /* class TradingDialogPage; */ 
-class MainWindow;
+class NewAccount;
 
 
 QT_BEGIN_NAMESPACE
@@ -80,7 +80,7 @@ private:
     GovernanceList *governanceListPage;      
     TransactionView *transactionView;
    /* TradingDialogPage *tradingDialogPage;  */
-    MainWindow *mainWindow;  
+    NewAccount *newAccount;  
 
     QProgressDialog *progressDialog;
     QLabel *transactionSum;
@@ -89,7 +89,7 @@ private:
 public Q_SLOTS:
 
     /** Switch to social media page */
-    void gotoMainWindow(); 
+    void gotoNewAccount(); 
     /** Switch to trading page */
  /*   void gotoTradingDialogPage();   */
     /** Switch to governance page */
