0b7f8ea144dc201dc1c1cc10300845fb6105c838
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-10 01:48:21 -0400

    Update profilepage.h

--- src/qt/profilepage.h
+++ src/qt/profilepage.h
@@ -1,22 +1,61 @@
 #ifndef PROFILEPAGE_H
 #define PROFILEPAGE_H
 
-#include <QDialog>
-
+#include <QMainWindow>
+#include "posts.h"
+#include "user.h"
+#include "homepage.h"
 namespace Ui {
-class profilepage;
+class ProfilePage;
 }
 
-class profilepage : public QDialog
+class ProfilePage : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit profilepage(QWidget *parent = 0);
-    ~profilepage();
+    explicit ProfilePage(QWidget *parent = 0);
+
+    ~ProfilePage();
+
+    ProfilePage(int userID,QString userFile);
+
+private Q_SLOTS:
+    void on_pushButton_2_clicked();
+
+    void viewPosts();
+
+    void on_LikeButton_clicked();
+
+    void viewMorePosts(int i);
+
+    void on_CommentButton_clicked();
+
+    void on_addFriendBtn_clicked();
+
+    void on_actionLog_out_triggered();
+
+
+public Q_SLOTS:
+    void setCurrentSessionUser_Ptr(user *currentSessionUser_ptr);
+
+    void on_Post_btn_clicked();
+
+    void setHomePageOwnerMail(QString owner);
+
+    void on_likesOwners_clicked();
+
 
 private:
-    Ui::profilepage *ui;
+    Ui::ProfilePage *ui;
+
+    unsigned int shownPostsNumber;
+
+    QList<Post> *pagePosts;
+
+    user *currentSessionUser;
+
+    QString homePageOwnerMail;
 };
 
 #endif // PROFILEPAGE_H
