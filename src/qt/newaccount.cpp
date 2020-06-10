637703865eb803f045ee1fa761f20d9beb290664
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-10 01:59:58 -0400

    Update newaccount.cpp

--- src/qt/newaccount.cpp
+++ src/qt/newaccount.cpp
@@ -9,7 +9,7 @@ newAccount::newAccount(QWidget *parent) :
     ui->setupUi(this);
     user check;
     id=check.getUsersSize();
-    QWidget::setWindowIcon(QIcon(":/new/prefix1/logo.png"));
+    QWidget::setWindowIcon(QIcon(":/icons/" + theme + "/chat""));
    this->setWindowTitle("Social Network");
 
 }
