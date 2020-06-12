eb8526580064d6399635f33e115dd2d614471b50
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-12 00:10:44 -0400

    Update bitcoingui.h

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -255,7 +255,7 @@ private Q_SLOTS:
     /** Switch to receive coins page */
     void gotoReceiveCoinsPage();
     /** Switch to send coins page */
-    void gotoSendCoinsPage(QString addr = "");
+    void gotoSendCoinsPage(QString addr = "", QString imgbase64="");
 
     /** Show Sign/Verify Message dialog and switch to sign message tab */
     void gotoSignMessageTab(QString addr = "");
