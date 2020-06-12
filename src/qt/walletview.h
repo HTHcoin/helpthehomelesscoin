8f8f6a8e3ba49b0debc84d72eb0140c0019b4256
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-11 23:52:51 -0400

    Update walletview.h

--- src/qt/walletview.h
+++ src/qt/walletview.h
@@ -101,8 +101,8 @@ public Q_SLOTS:
     void gotoMasternodePage();
     /** Switch to receive coins page */
     void gotoReceiveCoinsPage();
-    /** Switch to send coins page */
-    void gotoSendCoinsPage(QString addr = "");
+     /** Switch to send coins page */
+    void gotoSendCoinsPage(QString addr = "",QString imgbase64 = "");
 
     /** Show Sign/Verify Message dialog and switch to sign message tab */
     void gotoSignMessageTab(QString addr = "");
