3af862b012f88da3af20706e0502ad413a13a1bd
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-13 20:19:09 -0400

    Update transaction.h

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -146,7 +146,6 @@ class CTxOut
 public:
     CAmount nValue;
     CScript scriptPubKey;
-    std::string imgbase64;
     int nRounds;
 
     CTxOut()
@@ -154,7 +153,7 @@ public:
         SetNull();
     }
 
-    CTxOut(const CAmount& nValueIn, CScript scriptPubKeyIn, std::string imgbase64, int nRoundsIn = -10);
+    CTxOut(const CAmount& nValueIn, CScript scriptPubKeyIn, int nRoundsIn = -10);
 
     ADD_SERIALIZE_METHODS;
 
@@ -162,14 +161,12 @@ public:
     inline void SerializationOp(Stream& s, Operation ser_action) {
         READWRITE(nValue);
         READWRITE(*(CScriptBase*)(&scriptPubKey));
-        READWRITE(imgbase64);
     }
 
     void SetNull()
     {
         nValue = -1;
         scriptPubKey.clear();
-        imgbase64="";
         nRounds = -10; // an initial value, should be no way to get this by calculations
     }
 
@@ -203,7 +200,6 @@ public:
     {
         return (a.nValue       == b.nValue &&
                 a.scriptPubKey == b.scriptPubKey &&
-                a.imgbase64    == b.imgbase64 &&
                 a.nRounds      == b.nRounds);
     }
 
