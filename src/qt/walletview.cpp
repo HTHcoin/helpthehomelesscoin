957b0740c388369e97088cf57f679175f0aee6e7
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-14 21:28:46 -0400

    Update walletview.cpp

--- src/qt/walletview.cpp
+++ src/qt/walletview.cpp
@@ -178,7 +178,7 @@ void WalletView::setWalletModel(WalletModel *_walletModel)
         masternodeListPage->setWalletModel(_walletModel);
     }
     governanceListPage->setWalletModel(_walletModel);
-    proposalAdd->setModel(walletModel);
+    /*proposalAdd->setModel(walletModel);*/
     receiveCoinsPage->setModel(_walletModel);
     sendCoinsPage->setModel(_walletModel);
     usedReceivingAddressesPage->setModel(_walletModel->getAddressTableModel());
