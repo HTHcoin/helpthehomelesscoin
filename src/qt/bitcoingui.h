d58cbb131858446eb44fc289d7053c71f0127d3d
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-14 23:23:33 -0400

    Update bitcoingui.h

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -233,7 +233,7 @@ private Q_SLOTS:
 #ifdef ENABLE_WALLET
  	
      /** Switch to add proposal page */
-    void gotoProposalAddPage();
+    void gotoProposalPage();
     /** Switch to trading page */
  /*   void gotoTradingDialogPage();  */
     /** Switch to governance page */
