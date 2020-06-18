0554d5f3a4561d6e3818241e01c21730ee9ed944
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 17:10:41 -0400

    Update mainwindow.cpp

--- src/qt/mainwindow.cpp
+++ src/qt/mainwindow.cpp
@@ -1,45 +1,118 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include "secdialog.h"
-#include <QDebug>
-#include "homepage.h"
-#include "QMessageBox"
-#include <QDialog>
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <string>
+using namespace std;
 
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include <QMessageBox>
-#include <QPixmap>
+class User {
 
-MainWindow::MainWindow(QWidget *parent) :
-     QMainWindow(parent),
-        ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-    QPixmap pix(":/icons/chat.png");
-    int w = ui->label_pic->width();
-    int h = ui->label_pic->height();
-    ui->label_pic->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
- 
-}
- 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
- 
-void MainWindow::on_pushButton_Login_clicked()
-{
-    QString username = ui->lineEdit_username->text();
-    QString password = ui->lineEdit_password->text();
- 
-    if(username ==  "test" && password == "test") {
-        QMessageBox::information(this, "Login", "Username and password is correct");
-        //hide();
-        secDialog = new SecDialog(this);
-        secDialog->show();
+public:
+    string username;
+    string password;
+    string inputUsername;
+    string inputPassword;
+    
+    // User registration
+    void userRegisterDo()
+    {
+
+        ofstream usersFile ("userData.txt");
+
+        if (	!usersFile.is_open())
+        {
+            usersFile.open("userData.txt");
+        }
+
+            usersFile << username << " " << password << endl;
+
+            usersFile.close();
     }
+
+    // Initialize user registration
+    void userRegister()
+    {
+        cout << "Welcome!\n-------------------------\n\nPlease register.\nEnter a new username:\n";
+        cin >> username;
+
+        cout << "\nPlease enter a new password:\n";
+        cin >> password;
+
+        userRegisterDo();
+    }
+
+    // User login function
+    void login()
+    {
+        cout << "Please enter your username:\n";
+        cin >> inputUsername;
+
+        cout << "\nPlease enter your password:\n";
+        cin >> inputPassword;
+
+        string userAndPass = inputUsername + " " + inputPassword; // Search pattern
+        int offset; 
+        string line;
+        ifstream usersFile;
+        usersFile.open ("userData.txt");
+
+        if(usersFile.is_open())
+        {
+            while(!usersFile.eof())
+            {
+                getline(usersFile,line);
+                if ((offset = line.find(userAndPass, 0)) != string::npos) {	  // if login data is found
+                    cout << "found " << userAndPass << endl;
+                }
+
+                else	// if login data is not found
+                {
+                    cout << "\nUsername and/or password incorrect!\n\n\n\n";
+                    login();
+                }
+            }
+            
+			usersFile.close();
+        }
+        else
+            cout << "Unable to open userData.txt file." << endl;
+    }
+
+};
+
+// Main program
+int main() {
+    
+    User user1;
+    ifstream usersFile("userData.txt");
+    long begin, end;
+
+    if (usersFile.good())
+    {
+        cout << "File userData.txt found!\n\n";
+    }
+    
     else {
-        QMessageBox::warning(this,"Login", "Username and password is not correct");
+        user1.userRegister();
     }
+
+    if(usersFile.is_open())
+    {
+        begin = usersFile.tellg();
+        usersFile.seekg (0, ios::end);
+
+        end = usersFile.tellg();
+        usersFile.close();
+
+        if(begin == end)
+        {
+            user1.userRegister();
+        }
+
+        else
+            {
+                user1.login();
+            }
+    }
+
+    getch();
 }
