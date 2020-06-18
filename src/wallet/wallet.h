bbd287bcf4d260aa31b2b3105e5d87aa2bb5cbd9
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 02:53:54 -0400

    Update wallet.h

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -67,6 +67,8 @@ static const bool DEFAULT_SEND_FREE_TRANSACTIONS = false;
 static const bool DEFAULT_WALLET_REJECT_LONG_CHAINS = false;
 //! -txconfirmtarget default
 static const unsigned int DEFAULT_TX_CONFIRM_TARGET = 6;
+//! Largest (in bytes) free transaction we're willing to create
+static const unsigned int MAX_FREE_TRANSACTION_CREATE_SIZE = 1000;
 static const bool DEFAULT_WALLETBROADCAST = true;
 static const bool DEFAULT_DISABLE_WALLET = false;
 
