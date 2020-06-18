26b163af6a4e6bae5a9134067e1b4454c595cbd1
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 00:17:57 -0400

    Update newaccount.cpp

--- src/qt/newaccount.cpp
+++ src/qt/newaccount.cpp
@@ -1,55 +1,14 @@
-#include "newaccount.h"
-#include "ui_newaccount.h"
-#include "QMessageBox"
-#include<user.h>
-newAccount::newAccount(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::newAccount)
+#include "secdialog.h"
+#include "ui_secdialog.h"
+ 
+SecDialog::SecDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::SecDialog)
 {
     ui->setupUi(this);
-    user check;
-    id=check.getUsersSize();
-    QWidget::setWindowIcon(QIcon(":/icons/about.png"));
-   this->setWindowTitle("Social Network");
-
 }
-
-newAccount::~newAccount()
+ 
+SecDialog::~SecDialog()
 {
     delete ui;
-}
-
-void newAccount::on_pushButton_clicked()
-{
-    QString password =ui->lineEdit_2->text();
-    QString confirm =ui->lineEdit_3->text();
-    QString mail=ui->txtUserMail->text();
-    if(password!=confirm)
-    {
-        QMessageBox :: information(this,"confirmation","your password isn't confirmed try again");
-    }
-    else
-    {
-        user temp (password,mail,ui->txtUserName->text(),id);
-        temp.setUsersList(temp);
-        MainWindow* newWindow= new MainWindow(id);
-        newWindow->show();
-        this->hide();
-//        temp.userFileManipulator.updateActivity(mail,0,0);
-    }
-
-}
-
-void newAccount::setLoginPtr(MainWindow *ptr)
-{
-    this->mainWindowPtr = ptr;
-}
-
-void newAccount::on_pushButton_2_clicked()
-{
-    MainWindow* newWindow= new MainWindow(id);
-    newWindow->show();
-   // mainWindowPtr->show();
-    this->hide();
-
 }
