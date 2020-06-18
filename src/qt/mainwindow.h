4a6d6b66e2428e3e5c569ca4cf748736ddb47bdd
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 19:30:17 -0400

    Update mainwindow.h

--- src/qt/mainwindow.h
+++ src/qt/mainwindow.h
@@ -1,52 +1,15 @@
-#include<iostream>
-#include<string.h>
-#include<stdio.h>
-#include<ctype.h>
-
-using namespace std;
-
-int main()
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+ 
+#include <QMainWindow>
+ 
+class MainWindow : public QMainWindow
 {
-     char uname[50];
-     char pass[20];
-     cout<<"\n Enter User Name  :  ";
-     gets(uname);
-     cout<<"\n Enter Password   :  ";
-     gets(pass);
-     try
-     {
-          //This section is used to verify the number of characters.
-          if(strlen(pass)<6)
-          {
-               cout<<"\n Password must be at least 6 Characters Long..."<<endl;
-               throw 'c';
-          }
-          //This section is used to verify the digits in the string.
-          bool digit_yes=false;
-          bool valid;
-          int len = strlen(pass);
-          for (int count = 0; count < len; count++)
-          if (isdigit(pass[count]))      
-               digit_yes=true;
-          if (!digit_yes)
-          {
-               valid=false;
-               cout <<"\n Password must have at least One Digit (0-9)"<< endl;
-               throw 'c';
-          }
-          else
-          {
-               valid=true;
-               cout<<"\n Password is Correct";
-          }
-     }
-     catch(char c)
-     {
-          cout<<"\n Invalid Password Format!!!";
-     }
-     catch(...)
-     {
-          cout<<"\n Default Exception";
-     }
-     return 0;
-}
+    Q_OBJECT
+ 
+public:
+    MainWindow();
+    ~MainWindow();
+};
+ 
+#endif // MAINWINDOW_H
