56f90f887951a112080921278bbf179f6272bea9
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 19:17:09 -0400

    Update mainwindow.h

--- src/qt/mainwindow.h
+++ src/qt/mainwindow.h
@@ -1,59 +1,52 @@
-#include<iostream>//.h>
+#include<iostream>
 #include<string.h>
-//#include<conio.h>
+#include<stdio.h>
+#include<ctype.h>
 
-using namespace std; ///
+using namespace std;
 
-int main(){
-
-	char Username[30]="sammasangcap",Password[30]="abc123",username[30], password[30],age[2],name[60],sex[6],address[100];
-
-    unsigned int attempts = 0;
-    do
-    {
-        cout<<"\nUsername:";
-        cin>>username;
-
-        if(strlen(username) <4)
-        {
-            cout<<"\nUsername length must be atleast 4 characters";
-        }
-        else
-        {
-            cout<<"\nPassword:";
-            cin>>password;
-        }
-       
-
-        if(!( (strcmp(username,Username) == 0) && (strcmp(password,Password) == 0)))
-        {
-            cout<<"\nWrong username or password";
-            attempts++;
-            if(3 == attempts)
-            {
-                cout<<"You exceeded the number of times that you need to input the correct username and password. The system now is locked!";
-                return 0;
-            }
-
-        }
-        else
-        {
-            cout<<"\nLogin Successful!";
-            cout<<"\nPlease input the following information:";
-            cout<<"\nName";
-            cin>>name;
-            cout<<"\nAge:";
-            cin>>age;
-            cout<<"\nSex:";
-            cin>>sex;
-            cout<<"\nAddress:";
-            cin>>address;
-            break;
-        }
-    }while(1);
-
-	//getch();
-	return 0;
-
-
+int main()
+{
+     char uname[50];
+     char pass[20];
+     cout<<"\n Enter User Name  :  ";
+     gets(uname);
+     cout<<"\n Enter Password   :  ";
+     gets(pass);
+     try
+     {
+          //This section is used to verify the number of characters.
+          if(strlen(pass)<6)
+          {
+               cout<<"\n Password must be at least 6 Characters Long..."<<endl;
+               throw 'c';
+          }
+          //This section is used to verify the digits in the string.
+          bool digit_yes=false;
+          bool valid;
+          int len = strlen(pass);
+          for (int count = 0; count < len; count++)
+          if (isdigit(pass[count]))      
+               digit_yes=true;
+          if (!digit_yes)
+          {
+               valid=false;
+               cout <<"\n Password must have at least One Digit (0-9)"<< endl;
+               throw 'c';
+          }
+          else
+          {
+               valid=true;
+               cout<<"\n Password is Correct";
+          }
+     }
+     catch(char c)
+     {
+          cout<<"\n Invalid Password Format!!!";
+     }
+     catch(...)
+     {
+          cout<<"\n Default Exception";
+     }
+     return 0;
 }
