22356d5f258f3642aba1fa2166c3f45ace5c16c9
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 17:34:38 -0400

    Update mainwindow.h

--- src/qt/mainwindow.h
+++ src/qt/mainwindow.h
@@ -1,113 +1,61 @@
 #include <iostream>
 #include <fstream>
-#include <conio.h>
 #include <string>
+#include <cstdlib> // for exit(1);
+
 using namespace std;
 
-class User {
 
-public:
-    string username;
-    string password;
-    string inputUsername;
-    string inputPassword;
-    
-    // User registration
-    void userRegisterDo()
-    {
+void Login();
 
-        ofstream usersFile ("userData.txt");
+int main()
+{
+	Login();
+	return 0;
+}
 
-        if (	!usersFile.is_open())
-        {
-            usersFile.open("userData.txt");
-        }
-
-            usersFile << username << " " << password << endl;
-
-            usersFile.close();
-    }
-
-    // Initialize user registration
-    void userRegister()
-    {
-        cout << "Welcome!\n-------------------------\n\nPlease register.\nEnter a new username:\n";
-        cin >> username;
-
-        cout << "\nPlease enter a new password:\n";
-        cin >> password;
-
-        userRegisterDo();
-    }
-
-    // User login function
-    void login()
-    {
-        cout << "Please enter your username:\n";
-        cin >> inputUsername;
-
-        cout << "\nPlease enter your password:\n";
-        cin >> inputPassword;
-
-        string userAndPass = inputUsername + " " + inputPassword; // Search pattern
-        int offset; 
-        string line;
-        ifstream usersFile;
-        usersFile.open ("userData.txt");
-
-       bool found = false;
-      if(usersFile.is_open()) {
-          while(getline(usersFile,line) && !found) {
-              if (line.compare(userAndPass) == 0) { //match strings exactly!
-                  found = true; // found is true => break loop
-              }
-          }
-	usersFile.close(); //close the file before recursivly opening it again later
-	if(found) {
-		cout << "Welcome "<< inputUsername << '\n';
+void Login()
+{
+	char login_un[50], login_pw[50], username[50], password[50];
+	int c;
+	ifstream uin("user.txt");
+	ifstream pin("pass.txt");
+	
+	cout<<"Main\n\n"
+		<<"(1) Login\n"
+		<<"(2) Quit\n";
+	cin>> c;
+	
+	
+	
+	if (c==1)
+	{
+		uin.getline(username, 50);
+		while (strcmp(login_un, username) !=0)
+		{
+			cout<<"Username: ";
+			cin.getline(login_un, 50);
+			if (strcmp(login_un, username) ==0) break;
+			else
+				cout<<"\nInvalid Username.\n";
+		}
+		
+		pin.getline(password, 50);
+		while (strcmp(login_pw, password) !=0)
+		{
+			cout<<"\nPassword: ";
+			cin.getline(login_pw, 50);
+			if (strcmp(login_pw, password) ==0) break;
+			else
+				cout<<"\nInvalid Password\n";
+		}
+		
 	}
-	else {
-		cout << "\nUsername and/or password incorrect!\n\n\n\n";
-		login(); //ok we didn't find them, lets redue this!
+	
+	else if (c==2)
+	{
+		cout<<"Quitting\n";
+		exit(1);
 	}
-      }
-    }
-  }
-
-// Main program
-int main() {
-    
-    User user1;
-    ifstream usersFile("userData.txt");
-    long begin, end;
-
-    if (usersFile.good())
-    {
-        cout << "File userData.txt found!\n\n";
-    }
-    
-    else {
-        user1.userRegister();
-    }
-
-    if(usersFile.is_open())
-    {
-        begin = usersFile.tellg();
-        usersFile.seekg (0, ios::end);
-
-        end = usersFile.tellg();
-        usersFile.close();
-
-        if(begin == end)
-        {
-            user1.userRegister();
-        }
-
-        else
-            {
-                user1.login();
-            }
-    }
-
-    getch();
+	return;
 }
