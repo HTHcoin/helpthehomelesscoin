a06b5d417ed63ebcd15a1935bc44f1856047542a
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 00:16:06 -0400

    Update mainwindow.cpp

--- src/qt/mainwindow.cpp
+++ src/qt/mainwindow.cpp
@@ -1,66 +1,43 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "newaccount.h"
+#include "secdialg.h"
 #include <QDebug>
 #include "homepage.h"
 #include "QMessageBox"
 
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QPixmap>
 MainWindow::MainWindow(QWidget *parent) :
-QMainWindow(parent),
-ui(new Ui::MainWindow)
-{
-ui->setupUi(this);
-ui->signUpLabel->setText("<font color='red'>No account? Create one!</font>");
-QWidget::setWindowIcon(QIcon(":/new/prefix1/logo.png"));
-this->setWindowTitle("Social Network");
-
-}
-
-MainWindow::MainWindow(int userID) :
+    QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->signUpLabel->setText("<font color='red'>No account? Create one!</font>");
-    id=userID;
-    qDebug()<< "id is "<<id;
-
+    QPixmap pix(":/icons/chat.png");
+    int w = ui->label_pic->width();
+    int h = ui->label_pic->height();
+    ui->label_pic->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
+ 
 }
-
+ 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-void MainWindow::on_signUpButton_clicked()
+ 
+void MainWindow::on_pushButton_Login_clicked()
 {
-    newAccount *newAccountWindow = new newAccount;
-    newAccountWindow->show();
-    this->hide();
-
-
-}
-
-void MainWindow::on_logInButton_clicked()
-{
-    QString email=ui->txtUserMail->text();
-    QFile userFile("Users/"+email+".xml");
-    if(!userFile.open(QFile::ReadOnly))
-    {
-        QMessageBox::information(this,"Error","Email doesn't exist. Please sign up!");
-        return;
+    QString username = ui->lineEdit_username->text();
+    QString password = ui->lineEdit_password->text();
+ 
+    if(username ==  "test" && password == "test") {
+        QMessageBox::information(this, "Login", "Username and password is correct");
+        //hide();
+        secDialog = new SecDialog(this);
+        secDialog->show();
     }
-    user *currentSessionUser = new user();
-    currentSessionUser->userName = email;
-    currentSessionUser->userFileManipulator.name = email;
-    QString password = currentSessionUser->userFileManipulator.getPassword(email);
-    if(password != ui->txtPassword->text())
-    {
-        QMessageBox::information(this,"Error","Wrong Password. Please try again!");
-        return;
+    else {
+        QMessageBox::warning(this,"Login", "Username and password is not correct");
     }
-    HomePage *homePageWindow = new HomePage();
-    homePageWindow->setCurrentSessionUser_Ptr(currentSessionUser);
-    homePageWindow->show();
-    this->hide();
-
 }
