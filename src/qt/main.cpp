5b93ab735d2deaa4bf3dee5c9e10cde391de4fe3
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 19:30:49 -0400

    Create main.cpp

---
+++ src/qt/main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+#include "formlogin.h"
+#include <QApplication>
+ 
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow* mainWindow = new MainWindow();
+    FormLogin* formLogin = new FormLogin(mainWindow);
+    formLogin->show();
+    mainWindow->show();
+ 
+    return a.exec();
+}
