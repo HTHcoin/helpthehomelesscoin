c472723b48169d3263500f6b691e93f5a056174b
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 02:37:34 -0400

    Update wallet.h

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -61,6 +61,8 @@ static const CAmount MIN_CHANGE = CENT;
 static const CAmount MIN_FINAL_CHANGE = MIN_CHANGE/2;
 //! Default for -spendzeroconfchange
 static const bool DEFAULT_SPEND_ZEROCONF_CHANGE = true;
+//! Default for -sendfreetransactions
+static const bool DEFAULT_SEND_FREE_TRANSACTIONS = false;
 //! Default for -walletrejectlongchains
 static const bool DEFAULT_WALLET_REJECT_LONG_CHAINS = false;
 //! -txconfirmtarget default
