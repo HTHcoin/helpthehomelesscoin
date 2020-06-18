27b12a780b87a97343ff843ad24419a8027fa3a3
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 19:15:09 -0400

    Update mainwindow.h

--- src/qt/mainwindow.h
+++ src/qt/mainwindow.h
@@ -1,195 +1,59 @@
-#include<iostream>
-#include<istream>
-#include<fstream>
-#include<stdlib.h>
+#include<iostream>//.h>
 #include<string.h>
-using namespace std;
-void login();
-void registr();
-void forgot();
+//#include<conio.h>
 
-main()
-{
-        int choice;
-        cout<<"***********************************************************************\n\n\n";
-        cout<<"                      Welcome to login page                               \n\n";
-        cout<<"*******************        MENU        *******************************\n\n";
-        cout<<"1.LOGIN"<<endl;
-        cout<<"2.REGISTER"<<endl;
-        cout<<"3.FORGOT PASSWORD (or) USERNAME"<<endl;
-        cout<<"4.Exit"<<endl;
-        cout<<"\nEnter your choice :";
-        cin>>choice;
-        cout<<endl;
-        switch(choice)
-        {
-                case 1:
-                        login();
-                        break;
-                case 2:
-                        registr();
-                        break;
-                case 3:
-                        forgot();
-                        break;
-                case 4:
+using namespace std; ///
 
-                        cout<<"Thanks for using this program\nThis program is created by @harsha20599\n\n";
-                        break;
-                default:
-                        system("cls");
-                        cout<<"You've made a mistake , give it a try again\n"<<endl; 
-                        main();
-        }
-        
-}
+int main(){
 
-void login()
-{
-        int count;
-        string user,pass,u,p;
-        system("cls");
-        cout<<"please enter the following details"<<endl;
-        cout<<"USERNAME :";
-        cin>>user;
-        cout<<"PASSWORD :";
-        cin>>pass;
-        
-        ifstream input("database.txt");
-        while(input>>u>>p)
+	char Username[30]="sammasangcap",Password[30]="abc123",username[30], password[30],age[2],name[60],sex[6],address[100];
+
+    unsigned int attempts = 0;
+    do
+    {
+        cout<<"\nUsername:";
+        cin>>username;
+
+        if(strlen(username) <4)
         {
-                if(u==user && p==pass)
-        
-                {
-                        count=1;
-                        system("cls");
-                }
-        }
-        input.close();
-        if(count==1)
-        {
-                cout<<"\nHello"<<user<<"\nLOGIN SUCESS\nWe're glad that you're here.\nThanks for logging in\n";
-                cin.get();
-                cin.get();
-                main();
+            cout<<"\nUsername length must be atleast 4 characters";
         }
         else
         {
-                cout<<"\nLOGIN ERROR\nPlease check your username and password\n";
-                main();
+            cout<<"\nPassword:";
+            cin>>password;
         }
-}
+       
 
-void registr()
-{
-        
-        string reguser,regpass,ru,rp;
-        system("cls");
-        cout<<"Enter the username :";
-        cin>>reguser;
-        cout<<"\nEnter the password :";
-        cin>>regpass;
-        
-        ofstream reg("database.txt",ios::app);
-        reg<<reguser<<' '<<regpass<<endl;
-        system("cls");
-        cout<<"\nRegistration Sucessful\n";
-        main();
-        
-        
-}
-
-void forgot()
-{
-        int ch;
-        system("cls");
-        cout<<"Forgotten ? We're here for help\n";
-        cout<<"1.Search your id by username"<<endl;
-        cout<<"2.Search your id by password"<<endl;
-        cout<<"3.Main menu"<<endl;
-        cout<<"Enter your choice :";
-        cin>>ch;
-        switch(ch)
+        if(!( (strcmp(username,Username) == 0) && (strcmp(password,Password) == 0)))
         {
-                case 1:
-                {
-                        int count=0;
-                        string searchuser,su,sp;
-                        cout<<"\nEnter your remembered username :";
-                        cin>>searchuser;
-                        
-                        ifstream searchu("database.txt");
-                        while(searchu>>su>>sp)
-                        {
-                                if(su==searchuser)
-                                {
-                                        count=1;
-                                }
-                        }
-                        searchu.close();
-                        if(count==1)
-                        {
-                                cout<<"\n\nHurray, account found\n";
-                                cout<<"\nYour password is "<<sp;
-                                cin.get();
-                                cin.get();
-                                system("cls");
-                                main();
-                        }
-                        else
-                        {
-                                cout<<"\nSorry, Your userID is not found in our database\n";
-                                cout<<"\nPlease kindly contact your system administrator for more details \n";
-                                cin.get();
-                                cin.get();
-                                main();
-                        }
-                        break;
-                }
-                case 2:
-                {
-                        int count=0;
-                        string searchpass,su2,sp2;
-                        cout<<"\nEnter the remembered password :";
-                        cin>>searchpass;
-                        
-                        ifstream searchp("database.txt");
-                        while(searchp>>su2>>sp2)
-                        {
-                                if(sp2==searchpass)
-                                {
-                                        count=1;
-                                }       
-                        }
-                        searchp.close();
-                        if(count==1)
-                        {
-                                cout<<"\nYour password is found in the database \n";
-                                cout<<"\nYour Id is : "<<su2;
-                                cin.get();
-                                cin.get();
-                                system("cls");
-                                main();
-                        }
-                        else
-                        {
-                                cout<<"Sorry, We cannot found your password in our database \n";
-                                cout<<"\nkindly contact your administrator for more information\n";
-                                cin.get();
-                                cin.get();
-                                main();
-                        }
-                
-                        break;
-                }
-                
-                case 3:
-                {
-                        cin.get();
-                        main();
-                }
-                default:
-                        cout<<"Sorry, You entered wrong choice. Kindly try again"<<endl;
-                        forgot();
+            cout<<"\nWrong username or password";
+            attempts++;
+            if(3 == attempts)
+            {
+                cout<<"You exceeded the number of times that you need to input the correct username and password. The system now is locked!";
+                return 0;
+            }
+
         }
+        else
+        {
+            cout<<"\nLogin Successful!";
+            cout<<"\nPlease input the following information:";
+            cout<<"\nName";
+            cin>>name;
+            cout<<"\nAge:";
+            cin>>age;
+            cout<<"\nSex:";
+            cin>>sex;
+            cout<<"\nAddress:";
+            cin>>address;
+            break;
+        }
+    }while(1);
+
+	//getch();
+	return 0;
+
+
 }
