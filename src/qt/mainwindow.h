0f3f2dcdfedeccf32216aedd75fe8911add64efb
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 23:14:31 -0400

    Update mainwindow.h

--- src/qt/mainwindow.h
+++ src/qt/mainwindow.h
@@ -12,7 +12,9 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+   explicit MainWindow(QWidget *parent = 0);
+    MainWindow(int userID );
+    int id;
     ~MainWindow();
 
 private Q_SLOTS:
