0deae2c4c10412ead24924df9f81d2dd2c6835b1
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-18 21:38:46 -0400

    Update walletframe.h

--- src/qt/walletframe.h
+++ src/qt/walletframe.h
@@ -67,7 +67,7 @@ public Q_SLOTS:
     /** Switch to social media page */
     void gotoGovernancePage(); 
     /** Switch to social media page */
-    void gotoFormLogin(); 
+    void gotoLoginSystem(); 
     /** Switch to private send page */
     void gotoPrivateSendPage();
     /** Switch to overview (home) page */
