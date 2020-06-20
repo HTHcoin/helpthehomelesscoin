21f80ca396f73866ceb1ba92b6c6c3441f22f324
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-20 02:12:18 -0400

    Update profilepage.h

--- src/qt/profilepage.h
+++ src/qt/profilepage.h
@@ -3,8 +3,6 @@
 
 #include <QMainWindow>
 #include "posts.h"
-#include "user.h"
-#include "homepage.h"
 namespace Ui {
 class ProfilePage;
 }
@@ -15,47 +13,20 @@ class ProfilePage : public QMainWindow
 
 public:
     explicit ProfilePage(QWidget *parent = 0);
-
     ~ProfilePage();
-
-    ProfilePage(int userID,QString userFile);
 
 private Q_SLOTS:
     void on_pushButton_2_clicked();
-
     void viewPosts();
-
+    std::vector<Post>* makePosts();
     void on_LikeButton_clicked();
-
     void viewMorePosts(int i);
-
     void on_CommentButton_clicked();
-
-    void on_addFriendBtn_clicked();
-
-    void on_actionLog_out_triggered();
-
-
-public Q_SLOTS:
-    void setCurrentSessionUser_Ptr(user *currentSessionUser_ptr);
-
-    void on_Post_btn_clicked();
-
-    void setHomePageOwnerMail(QString owner);
-
-    void on_likesOwners_clicked();
-
 
 private:
     Ui::ProfilePage *ui;
-
     unsigned int shownPostsNumber;
-
-    QList<Post> *pagePosts;
-
-    user *currentSessionUser;
-
-    QString homePageOwnerMail;
+    std::vector<Post> *pagePosts;
 };
 
 #endif // PROFILEPAGE_H
