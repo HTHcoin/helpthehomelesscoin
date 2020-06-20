f0cc7deb2fdb95448659a1630b23f2ee38f04b98
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-19 21:09:30 -0400

    Update main.cpp

--- src/qt/main.cpp
+++ src/qt/main.cpp
@@ -1,15 +1,12 @@
-  
 #include "mainwindow.h"
-#include "formlogin.h"
 #include <QApplication>
- 
+#include "homepage.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow* mainWindow = new MainWindow();
-    FormLogin* formLogin = new FormLogin(mainWindow);
-    formLogin->show();
-    mainWindow->show();
- 
+    MainWindow w;
+    w.show();
+
     return a.exec();
 }
