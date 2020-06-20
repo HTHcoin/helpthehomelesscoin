6e6fdeb563bcba40013ad402844353b0cfc22239
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-19 22:07:47 -0400

    Update mainwindow.cpp

--- src/qt/mainwindow.cpp
+++ src/qt/mainwindow.cpp
@@ -1,68 +1,82 @@
+#ifndef _MainWindow_CPP
+#define	_MainWindow_CPP
+
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include "newaccount.h"
-#include  <QDebug>
-#include <QFile>
-#include <QString>
-#include "homepage.h"
-#include <QMessageBox>
 
-MainWindow::MainWindow(QWidget *parent) :
-QMainWindow(parent),
-ui(new Ui::MainWindow)
+MainWindow::MainWindow()
 {
-ui->setupUi(this);
-ui->signUpLabel->setText("<font color='red'>No account? Create one!</font>");
-QWidget::setWindowIcon(QIcon(":/icons/about.png"));
-this->setWindowTitle("Social Network");
-
+    setupUi(this);
+    connect(loginbtn, SIGNAL(clicked()), this, SLOT(loginclick()));
+    connect(quitbtn, SIGNAL(clicked()), this, SLOT(close()));
 }
 
-MainWindow::MainWindow(int userID) :
-    ui(new Ui::MainWindow)
+void MainWindow::setupUi(QDialog *MainWindow)
 {
-    ui->setupUi(this);
-    ui->signUpLabel->setText("<font color='red'>No account? Create one!</font>");
-    id=userID;
-    qDebug()<< "id is "<<id;
+    if (MainWindow->objectName().isEmpty())
+        MainWindow->setObjectName(QString::fromUtf8("LoginForm"));
+    MainWindow->setWindowModality(Qt::NonModal);
+    MainWindow->resize(627, 414);
+    loginbtn = new QPushButton(LoginForm);
+    loginbtn->setObjectName(QString::fromUtf8("loginbtn"));
+    loginbtn->setGeometry(QRect(140, 250, 114, 32));
+    quitbtn = new QPushButton(LoginForm);
+    quitbtn->setObjectName(QString::fromUtf8("quitbtn"));
+    quitbtn->setGeometry(QRect(300, 250, 114, 32));
+    usernametext = new QLineEdit(LoginForm);
+    usernametext->setObjectName(QString::fromUtf8("usernametext"));
+    usernametext->setGeometry(QRect(240, 140, 113, 22));
+    passtext = new QLineEdit(LoginForm);
+    passtext->setObjectName(QString::fromUtf8("passtext"));
+    passtext->setGeometry(QRect(240, 190, 113, 22));
+    passtext->setInputMask(QString::fromUtf8(""));
+    passtext->setMaxLength(32767);
+    passtext->setEchoMode(QLineEdit::Password);
+    password = new QLabel(LoginForm);
+    password->setObjectName(QString::fromUtf8("password"));
+    password->setGeometry(QRect(130, 190, 62, 16));
+    username = new QLabel(LoginForm);
+    username->setObjectName(QString::fromUtf8("username"));
+    username->setGeometry(QRect(130, 140, 62, 16));
 
-}
+    retranslateUi(LoginForm);
+
+    QMetaObject::connectSlotsByName(LoginForm);
+} // setupUi
+
+void MainWindow::retranslateUi(QDialog *MainWindow)
+{
+    MainWindow->setWindowTitle(QApplication::translate("LoginForm", "User System Login", 0, QApplication::UnicodeUTF8));
+    loginbtn->setText(QApplication::translate("LoginForm", "Login", 0, QApplication::UnicodeUTF8));
+    quitbtn->setText(QApplication::translate("LoginForm", "Quit", 0, QApplication::UnicodeUTF8));
+
+#ifndef QT_NO_TOOLTIP
+    usernametext->setToolTip(QApplication::translate("LoginForm", "Enter Username", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+
+#ifndef QT_NO_TOOLTIP
+    passtext->setToolTip(QApplication::translate("LoginForm", "Enter Password", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+
+    passtext->setText(QString());
+    passtext->setPlaceholderText(QString());
+    password->setText(QApplication::translate("LoginForm", "Password", 0, QApplication::UnicodeUTF8));
+    username->setText(QApplication::translate("LoginForm", "Username", 0, QApplication::UnicodeUTF8));
+} // retranslateUi
 
 MainWindow::~MainWindow()
 {
-    delete ui;
 }
 
-void MainWindow::on_signUpButton_clicked()
+void MainWindow::loginclick()
 {
-    newAccount *newAccountWindow = new newAccount;
-    newAccountWindow->show();
-    this->hide();
-
-
+    if (usernametext->text() == "Devilking6105" && passtext->text() == "123456")
+    {
+        QMessageBox::information(this, "Success", "Password Correct");
+    }
+    else
+    {
+        QMessageBox::information(this, "Failure", "Password Incorrect");
+    }
 }
 
-void MainWindow::on_logInButton_clicked()
-{
-    QString email=ui->txtUserMail->text();
-    QFile userFile("Users/"+email+".xml");
-    if(!userFile.open(QFile::ReadOnly))
-    {
-        QMessageBox::information(this,"Error","Email doesn't exist. Please sign up!");
-        return;
-    }
-    user *currentSessionUser = new user();
-    currentSessionUser->userName = email;
-    currentSessionUser->userFileManipulator.name = email;
-    QString password = currentSessionUser->userFileManipulator.getPassword(email);
-    if(password != ui->txtPassword->text())
-    {
-        QMessageBox::information(this,"Error","Wrong Password. Please try again!");
-        return;
-    }
-    HomePage *homePageWindow = new HomePage();
-    homePageWindow->setCurrentSessionUser_Ptr(currentSessionUser);
-    homePageWindow->show();
-    this->hide();
-
-}
+#endif
