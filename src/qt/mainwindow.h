ad8a8f8babe7988ae24a1f22a85afcc632a9c725
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-19 21:11:44 -0400

    Update mainwindow.h

--- src/qt/mainwindow.h
+++ src/qt/mainwindow.h
@@ -1,15 +1,29 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
- 
+
 #include <QMainWindow>
- 
+
+namespace Ui {
+class MainWindow;
+}
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
- 
+
 public:
-    MainWindow();
+   explicit MainWindow(QWidget *parent = 0);
+    MainWindow(int userID );
+    int id;
     ~MainWindow();
+
+private Q_SLOTS:
+    void on_signUpButton_clicked();
+
+    void on_logInButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
 };
- 
+
 #endif // MAINWINDOW_H
