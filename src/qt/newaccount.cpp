b1232f28b976e9b2a9bb72e7f7ab6113865e0930
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-19 23:34:26 -0400

    Update newaccount.cpp

--- src/qt/newaccount.cpp
+++ src/qt/newaccount.cpp
@@ -1,19 +1,11 @@
 #include "newaccount.h"
 #include "ui_newaccount.h"
-#include <QMessageBox>
-#include <QString>
-#include <QFile>
-#include<user.h>
+#include "QMessageBox"
 newAccount::newAccount(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::newAccount)
 {
     ui->setupUi(this);
-    user check;
-    id=check.getUsersSize();
-    QWidget::setWindowIcon(QIcon(":/icons/chat.png"));
-   this->setWindowTitle("Social Network");
-
 }
 
 newAccount::~newAccount()
@@ -25,21 +17,10 @@ void newAccount::on_pushButton_clicked()
 {
     QString password =ui->lineEdit_2->text();
     QString confirm =ui->lineEdit_3->text();
-    QString mail=ui->txtUserMail->text();
     if(password!=confirm)
     {
         QMessageBox :: information(this,"confirmation","your password isn't confirmed try again");
     }
-    else
-    {
-        user temp (password,mail,ui->txtUserName->text(),id);
-        temp.setUsersList(temp);
-        MainWindow* newWindow= new MainWindow(id);
-        newWindow->show();
-        this->hide();
-        temp.userFileManipulator.updateActivity(mail,0,0);
-    }
-
 }
 
 void newAccount::setLoginPtr(MainWindow *ptr)
@@ -49,9 +30,6 @@ void newAccount::setLoginPtr(MainWindow *ptr)
 
 void newAccount::on_pushButton_2_clicked()
 {
-    MainWindow* newWindow= new MainWindow(id);
-    newWindow->show();
-   // mainWindowPtr->show();
+    mainWindowPtr->show();
     this->hide();
-
 }
