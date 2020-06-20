6b38ed7e5afebcaab2b7fe003045023a343f669c
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-06-19 21:10:59 -0400

    Update mainwindow.cpp

--- src/qt/mainwindow.cpp
+++ src/qt/mainwindow.cpp
@@ -1,9 +1,66 @@
 #include "mainwindow.h"
- 
-MainWindow::MainWindow()
-    : QMainWindow()
+#include "ui_mainwindow.h"
+#include "newaccount.h"
+#include  <QDebug>
+#include "homepage.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+QMainWindow(parent),
+ui(new Ui::MainWindow)
 {
-    setMinimumSize(800, 600);
+ui->setupUi(this);
+ui->signUpLabel->setText("<font color='red'>No account? Create one!</font>");
+QWidget::setWindowIcon(QIcon(":/icons/about.png"));
+this->setWindowTitle("Social Network");
+
 }
- 
-MainWindow::~MainWindow() {}
+
+MainWindow::MainWindow(int userID) :
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->signUpLabel->setText("<font color='red'>No account? Create one!</font>");
+    id=userID;
+    qDebug()<< "id is "<<id;
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_signUpButton_clicked()
+{
+    newAccount *newAccountWindow = new newAccount;
+    newAccountWindow->show();
+    this->hide();
+
+
+}
+
+void MainWindow::on_logInButton_clicked()
+{
+    QString email=ui->txtUserMail->text();
+    QFile userFile("Users/"+email+".xml");
+    if(!userFile.open(QFile::ReadOnly))
+    {
+        QMessageBox::information(this,"Error","Email doesn't exist. Please sign up!");
+        return;
+    }
+    user *currentSessionUser = new user();
+    currentSessionUser->userName = email;
+    currentSessionUser->userFileManipulator.name = email;
+    QString password = currentSessionUser->userFileManipulator.getPassword(email);
+    if(password != ui->txtPassword->text())
+    {
+        QMessageBox::information(this,"Error","Wrong Password. Please try again!");
+        return;
+    }
+    HomePage *homePageWindow = new HomePage();
+    homePageWindow->setCurrentSessionUser_Ptr(currentSessionUser);
+    homePageWindow->show();
+    this->hide();
+
+}
