54d5500664e39842e6727351c616f608894453e3
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-07-11 19:31:20 -0400

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -30,6 +30,7 @@
 #include "validation.h"
 #include "wallet/wallet.h"
 #include "net.h"
+#include "newsitem.h"
 
 #include "instantx.h"
 #include "masternode-sync.h"
@@ -57,6 +58,8 @@
 #define NUM_ITEMS 5
 #define NUM_ITEMS_ADV 7
 
+#define NEWS_URL "https://reecore.org/category/news/feed"
+
 #include "overviewpage.moc"
 
 OverviewPage::OverviewPage(const PlatformStyle *platformStyle, QWidget *parent) :
@@ -78,6 +81,11 @@ OverviewPage::OverviewPage(const PlatformStyle *platformStyle, QWidget *parent) 
                
     ui->setupUi(this);
     QString theme = GUIUtil::getThemeName();
+	    
+    ui->listNews->setSortingEnabled(true);
+    ui->labelNewsStatus->setText("(" + tr("out of sync") + ")");
+
+    connect(&manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(newsFinished(QNetworkReply*)));	    
      
     ui->pushButton_Website->setStatusTip(tr("Visit Help The Homeless Worldwide A NJ Nonprofit Corporation"));
     ui->pushButton_Website_1->setStatusTip(tr("Visit Help The Homeless Coin"));
@@ -90,6 +98,12 @@ OverviewPage::OverviewPage(const PlatformStyle *platformStyle, QWidget *parent) 
     ui->labelWalletStatus->setText("(" + tr("out of sync") + ")");
   
     //information block update
+	    
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(updateNewsList()));
+    timer->setInterval(10 * 1000); // after 10 seconds on the 1st cycle
+    timer->setSingleShot(true);
+    timer->start();	    
    	    
     timerinfo_mn = new QTimer(this);
     connect(timerinfo_mn, SIGNAL(timeout()), this, SLOT(updateMasternodeInfo()));
@@ -331,3 +345,178 @@ void OverviewPage::on_pushButton_Mine_AMD_clicked()
 }
 
 /************** HTH Worldwide Button *****************/
+
+/**** News Section ****/
+
+void OverviewPage::updateNewsList()
+{
+    ui->labelNewsStatus->setVisible(true);
+
+    xml.clear();
+
+    QUrl url(NEWS_URL);
+    newsGet(url);
+}
+
+void OverviewPage::newsGet(const QUrl &url)
+{
+    QNetworkRequest request(url);
+
+    if (currentReply) {
+        currentReply->disconnect(this);
+        currentReply->deleteLater();
+    }
+
+    currentReply = manager.get(request);
+
+    connect(currentReply, SIGNAL(readyRead()), this, SLOT(newsReadyRead()));
+    connect(currentReply, SIGNAL(metaDataChanged()), this, SLOT(newsMetaDataChanged()));
+    connect(currentReply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(newsError(QNetworkReply::NetworkError)));
+}
+
+void OverviewPage::newsMetaDataChanged()
+{
+    QUrl redirectionTarget = currentReply->attribute(QNetworkRequest::RedirectionTargetAttribute).toUrl();
+    if (redirectionTarget.isValid()) {
+        newsGet(redirectionTarget);
+    }
+}
+
+void OverviewPage::newsReadyRead()
+{
+    int statusCode = currentReply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+
+    if (statusCode >= 200 && statusCode < 300) {
+        QByteArray data = currentReply->readAll();
+        xml.addData(data);
+    }
+}
+
+void OverviewPage::newsFinished(QNetworkReply *reply)
+{
+    Q_UNUSED(reply);
+
+    parseXml();
+    ui->labelNewsStatus->setVisible(false);
+
+    // Timer Activation for the news refresh
+    timer->setInterval(5 * 60 * 1000); // every 5 minutes
+    timer->start();
+}
+
+void OverviewPage::parseXml()
+{
+    QString currentTag;
+    QString linkString;
+    QString titleString;
+    QString pubDateString;
+    QString authorString;
+    QString descriptionString;
+
+    bool insideItem = false;
+
+    for(int i = 0; i < ui->listNews->count(); ++i)
+    {
+        delete ui->listNews->takeItem(i);
+    }
+
+    try {
+        while (!xml.atEnd()) {
+            xml.readNext();
+            if (xml.isStartElement()) {
+                currentTag = xml.name().toString();
+
+                if (xml.name() == "item")
+                {
+                    insideItem = true;
+                    titleString.clear();
+                    pubDateString.clear();
+                    authorString.clear();
+                    descriptionString.clear();
+                }
+            } else if (xml.isEndElement()) {
+                if (xml.name() == "item") {
+                    if( !linkString.isEmpty() && !linkString.isNull()
+                     && !titleString.isEmpty() && !titleString.isNull()
+                     && !authorString.isEmpty() && !authorString.isNull()
+                     && !pubDateString.isEmpty() && !pubDateString.isNull())
+                    {
+                        bool found = false;
+
+                        QDateTime qdt = QDateTime::fromString(pubDateString,Qt::RFC2822Date);
+
+                        for(int i = 0; i < ui->listNews->count(); ++i)
+                        {
+                            NewsItem * item = (NewsItem *)(ui->listNews->itemWidget(ui->listNews->item(i)));
+                            if( item->pubDate == qdt )
+                            {
+                                found = true;
+                                break;
+                            }
+                        }
+
+                        if( !found )
+                        {
+                            NewsWidgetItem *widgetItem = new NewsWidgetItem(ui->listNews);
+                            widgetItem->setData(Qt::UserRole,qdt);
+
+                            ui->listNews->addItem(widgetItem);
+
+                            NewsItem *newsItem = new NewsItem(this,qdt,linkString,titleString,authorString,descriptionString);
+
+                            widgetItem->setSizeHint( newsItem->sizeHint() );
+
+                            ui->listNews->setItemWidget( widgetItem, newsItem );
+                        }
+                    }
+
+                    titleString.clear();
+                    linkString.clear();
+                    pubDateString.clear();
+                    authorString.clear();
+                    descriptionString.clear();
+
+                    insideItem = false;
+                }
+            } else if (xml.isCharacters() && !xml.isWhitespace()) {
+                if (insideItem) {
+                    if (currentTag == "title")
+                        titleString += xml.text().toString();
+                    else if (currentTag == "link")
+                        linkString += xml.text().toString();
+                    else if (currentTag == "pubDate")
+                        pubDateString += xml.text().toString();
+                    else if (currentTag == "creator")
+                        authorString += xml.text().toString();
+                    else if (currentTag == "description")
+                        descriptionString += xml.text().toString();
+                }
+            }
+        }
+
+        if (xml.error() && xml.error() != QXmlStreamReader::PrematureEndOfDocumentError) {
+            qWarning() << "XML ERROR:" << xml.lineNumber() << ": " << xml.errorString();
+        }
+    }
+
+    catch(std::exception &e)
+    {
+        qWarning("std:exception %s",e.what());
+    }
+
+    catch(...)
+    {
+        qWarning("generic exception");
+    }
+}
+
+void OverviewPage::newsError(QNetworkReply::NetworkError)
+{
+    qWarning("error retrieving RSS feed");
+
+    currentReply->disconnect(this);
+    currentReply->deleteLater();
+    currentReply = 0;
+}
+
+/*** End News Section ***/
