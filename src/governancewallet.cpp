bd47c2f55d9884e3886c343e195e4286ab0bf00f
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-07-10 21:27:52 -0400

    Update governancewallet.cpp

--- src/governancewallet.cpp
+++ src/governancewallet.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <governance/governancewallet.h>
+#include <governancewallet.h>
 
 #include <wallet/coincontrol.h>
 #include <wallet/fees.h>
