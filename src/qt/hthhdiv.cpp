a502afaaa4035982935351f757975ba03903d0cf
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-07-10 21:05:34 -0400

    Update and rename blocknethdiv.cpp to hthhdiv.cpp

--- src/qt/hthhdiv.cpp
+++ src/qt/hthhdiv.cpp
@@ -2,9 +2,9 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <qt/blocknethdiv.h>
+#include <qt/hthhdiv.h>
 
-BlocknetHDiv::BlocknetHDiv(QLabel *parent) : QLabel(parent) {
+HTHHDiv::HTHHDiv(QLabel *parent) : QLabel(parent) {
     this->setObjectName("hdiv");
     this->setFixedHeight(1);
     this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
