bf76b5a6f2946d69b671255a6f509098999bb2ee
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-07-12 20:10:18 -0400

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -58,7 +58,7 @@
 #define NUM_ITEMS 5
 #define NUM_ITEMS_ADV 7
 
-#define NEWS_URL "https://www.yahoo.com/lifestyle/tagged/homeless/"
+#define NEWS_URL "https://reecore.org/category/news/feed"
 
 #include "overviewpage.moc"
 
