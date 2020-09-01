fcc35e3688c2c32777842e4e5e34a538f7d5a638
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-08-31 20:32:49 -0400

    Update build-docker.sh

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -3,18 +3,18 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
-DOCKER_IMAGE=${DOCKER_IMAGE:-Help-The-Homeless-Coin-0.14/HelpTheHomelessd-develop}
+DOCKER_IMAGE=${DOCKER_IMAGE:-helpthehomeless/helpthehomelessd-develop}
 DOCKER_TAG=${DOCKER_TAG:-latest}
 
 BUILD_DIR=${BUILD_DIR:-.}
 
 rm docker/bin/*
 mkdir docker/bin
-cp $BUILD_DIR/src/HelpTheHomelessd docker/bin/
-cp $BUILD_DIR/src/HelpTheHomeless-cli docker/bin/
-cp $BUILD_DIR/src/HelpTheHomeless-tx docker/bin/
-strip docker/bin/HelpTheHomelessd
-strip docker/bin/HelpTheHomeless-cli
-strip docker/bin/HelpTheHomeless-tx
+cp $BUILD_DIR/src/helpthehomelessd docker/bin/
+cp $BUILD_DIR/src/helpthehomeless-cli docker/bin/
+cp $BUILD_DIR/src/helpthehomeless-tx docker/bin/
+strip docker/bin/helpthehomelessd
+strip docker/bin/helpthehomeless-cli
+strip docker/bin/helpthehomeless-tx
 
 docker build --pull -t $DOCKER_IMAGE:$DOCKER_TAG -f docker/Dockerfile docker
