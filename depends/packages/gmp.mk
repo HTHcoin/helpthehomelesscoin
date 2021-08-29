9fbea1f44a64ec4e601d474f19fec3b4c1b74486
root <root@devilking6105.localdomain>
2021-08-29 00:25:29 -0400

    I am a Droid

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,12 +1,18 @@
 package=gmp
 $(package)_version=6.1.2
-$(package)_download_path=https://gmplib.org/download/gmp
-$(package)_file_name=gmp-$($(package)_version).tar.bz2
-$(package)_sha256_hash=5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2
+$(package)_sha256_hash=87b565e89a9a684fe4ebeeddb8399dce2599f9c9049854ca8c0dfbdea0e21912
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
+$(package)_download_path=https://gmplib.org/download/$(package)
 
 define $(package)_set_vars
-$(package)_config_opts+=--enable-cxx --enable-fat --with-pic --disable-shared
-$(package)_cflags_armv7l_linux+=-march=armv7-a
+  $(package)_config_opts=--disable-shared --enable-cxx
+  $(package)_config_opts += --enable-option-checking
+  $(package)_config_opts_linux=--with-pic
+  $(package)_config_opts_darwin += --with-pic
+endef
+
+define $(package)_preprocess_cmds
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
 endef
 
 define $(package)_config_cmds
@@ -20,4 +26,3 @@ endef
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
-
